6895317d9d71d090f8a363654964b024461afa9d
Diorgenes Luan de Menezes Backes <diorgenes.backes@agibank.com.br>
2021-07-14 22:45:40 -0300

    feat: init project

---
+++ src/test/java/br/com/challenge/voteservice/VoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.voteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
