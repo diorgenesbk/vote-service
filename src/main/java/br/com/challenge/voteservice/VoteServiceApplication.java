2fccd1185903fceadf31854d31326988a2a02507
Diorgenes Luan de Menezes Backes <diorgenes.backes@agibank.com.br>
2021-07-17 04:24:09 -0300

    fix: clean blank spaces

--- src/main/java/br/com/challenge/voteservice/VoteServiceApplication.java
+++ src/main/java/br/com/challenge/voteservice/VoteServiceApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VoteServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(VoteServiceApplication.class, args);
 	}
-
-
 
 }
